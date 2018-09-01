07e9ce3e69d2ed1af5470b3e5f30441f1dbf7f89
FrencisAlreadyInUse <francisdclercq@gmail.com>
2018-09-01 18:46:09 +0200

    added stylelint rules

---
+++ README.md
@@ -0,0 +1,23 @@
+# stylelint-config-wild
+
+Centralized stylelint rules of wild
+
+## Installation
+
+Install this package, and save it as a devDependency:
+
+```
+npm i -D stylelint-config-devine
+```
+
+Then have your project's `.stylelintrc` file extend the ruleset.
+
+```json
+{
+  "extends": "stylelint-config-wild"
+}
+```
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = {
+
+  extends: [
+    `stylelint-config-standard`
+  ],
+
+  rules: {
+    "selector-list-comma-newline-after": null,
+    "block-closing-brace-empty-line-before": null,
+    "rule-empty-line-before": `always`,
+    "declaration-empty-line-before": null,
+    "font-family-name-quotes": `always-where-recommended`,
+    "color-named": `always-where-possible`,
+    "comment-empty-line-before": null,
+    "comment-whitespace-inside": null,
+    "function-url-quotes": `never`,
+    "string-quotes": `single`,
+    "declaration-no-important": true,
+    "no-duplicate-selectors": true,
+    "selector-max-id": 0,
+    "number-max-precision": 2,
+    "shorthand-property-no-redundant-values": true,
+    "selector-no-qualifying-type": [true, {ignore: `attribute`}],
+    "selector-max-universal": 0,
+    "selector-no-vendor-prefix": true,
+    "property-no-vendor-prefix": true,
+    "media-feature-name-case": `lower`,
+    "number-no-trailing-zeros": true,
+    "number-leading-zero": `never`,
+    "value-keyword-case": `lower`,
+    "no-extra-semicolons": true,
+    "declaration-block-no-shorthand-property-overrides": true,
+    "declaration-block-no-redundant-longhand-properties": [true, {ignoreShorthands: ["grid-template"]}],
+  }
+
+};
